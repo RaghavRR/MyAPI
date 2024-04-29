05bc04087350d94d11e079a77658edfb6699484c
Raghav Rastogi <raghavrastogi2021@gmail.com>
2024-04-29 19:53:05 +0530

    ADDED

---
+++ MyAI/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TextGenration from './Components/TextGenration'
+
+const App = () => {
+  return (
+    <>
+    <TextGenration />
+    </>
+  )
+}
+
+export default App
